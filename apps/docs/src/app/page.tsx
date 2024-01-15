fc468b6221946849862211995101f7caabea339d
Raju-github-profile <rajukadel.github@gmail.com>
2024-01-15 08:56:41 +0545

    added docs inside apps

---
+++ apps/docs/src/app/page.tsx
@@ -0,0 +1,40 @@
+'use client'
+import { useCallback, useState } from 'react';
+import ReactFlow, { addEdge, applyEdgeChanges, applyNodeChanges } from 'reactflow';
+import 'reactflow/dist/style.css';
+
+import initialNodes from './node';
+import initialEdges from './edge';
+
+function Flow() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges]
+  );
+  const onConnect = useCallback(
+    (connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges]
+  );
+
+  return (
+    <div className='h-[100vh] w-[100vw]'>
+      <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      fitView
+    />
+    </div>
+  );
+}
+
+export default Flow;
