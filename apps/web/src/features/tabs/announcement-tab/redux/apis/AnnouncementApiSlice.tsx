6aac6769e0cbbec805e1214f97ec5863ac3cebbf
Raju-github-profile <rajukadel.github@gmail.com>
2024-01-18 08:53:17 +0545

    feat: api slice for announcement tab

--- apps/web/src/features/tabs/announcement-tab/redux/apis/AnnouncementApiSlice.tsx
+++ apps/web/src/features/tabs/announcement-tab/redux/apis/AnnouncementApiSlice.tsx
@@ -4,11 +4,13 @@ export const AnnouncementApiSlice = apiSlice.injectEndpoints({
 
     endpoints: (builder: any) => ({
 
-        getAllAnnouncement: builder.mutation({
-            query: ({ channelId }: { channelId: string }) => ({
+        getAllAnnouncement: builder.query({
+
+            query: (channelId: string) => ({
                 url: `/api/announcement/getall/${channelId}`,
                 method: 'GET',
             }),
+
             async onQueryStarted(arg: unknown, { dispatch, queryFulfilled }: unknown) {
                 try {
                     console.log({ arg })
@@ -19,26 +21,36 @@ export const AnnouncementApiSlice = apiSlice.injectEndpoints({
                 } catch (error) {
                     console.log(error)
                 }
+            },
+
+            providesTags: (result: any, error: any, arg: any) => {
+                console.log({ result, error, arg })
+                return [{
+                    type: 'Announcement',
+                    id: 'LIST'
+                }]
+                //     if (result?.ids) {
+                //         return [
+                //             { type: 'User', id: 'LIST' },
+                //             ...result.ids.map(id => ({ type: 'User', id }))
+                //         ]
+                //     } else return [{ type: 'User', id: 'LIST' }]
+                // }
             }
+
         }),
 
         createAnnouncement: builder.mutation({
+
             query: (payload: any) => ({
                 url: '/api/announcement/create',
                 method: 'POST',
                 body: payload
             }),
-            async onQueryStarted(arg: any, { dispatch, queryFulfilled }: any) {
-                try {
-                    const data = await queryFulfilled;
-                    console.log({ data })
 
-                    // dispatch(handleUser(data));
-
-                } catch (error) {
-                    console.log(error)
-                }
-            }
+            invalidatesTags: (result: any, error: any, arg: any) => [
+                { type: 'Announcement', id: 'LIST' }
+            ],
         }),
 
         updateAnnouncement: builder.mutation({
@@ -47,6 +59,9 @@ export const AnnouncementApiSlice = apiSlice.injectEndpoints({
                 method: 'POST',
                 body: payload
             }),
+            invalidatesTags: (result: any, error: any, arg: any) => [
+                { type: 'Announcement', id: 'LIST' }
+            ],
             async onQueryStarted(arg: any, { dispatch, queryFulfilled }: any) {
                 try {
                     const data = await queryFulfilled;
@@ -60,10 +75,13 @@ export const AnnouncementApiSlice = apiSlice.injectEndpoints({
         }),
 
         deleteAnnouncement: builder.mutation({
-            query: () => ({
-                url: '/api/announcement/delete/:announcementId',
+            query: (announcementId: string) => ({
+                url: `/api/announcement/delete/${announcementId}`,
                 method: 'POST',
             }),
+            invalidatesTags: (result: any, error: any, arg: any) => [
+                { type: 'Announcement', id: 'LIST' }
+            ],
             async onQueryStarted(arg: any, { dispatch, queryFulfilled }: any) {
                 try {
                     const data = await queryFulfilled;
@@ -78,10 +96,9 @@ export const AnnouncementApiSlice = apiSlice.injectEndpoints({
     })
 });
 
-
 export const {
     useDeleteAnnouncementMutation,
     useGetAllAnnouncementQuery,
     useCreateAnnouncementMutation,
     useUpdateAnnouncementMutation
-} = AnnouncementApiSlice
+} = AnnouncementApiSlice;
