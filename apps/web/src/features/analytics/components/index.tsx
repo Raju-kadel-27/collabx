40541095c08d01c060a3925d94a60cf199d0074f
Raju-github-profile <rajukadel.github@gmail.com>
2024-01-31 18:39:19 +0545

    feat: added configs

---
+++ apps/web/src/features/analytics/components/index.tsx
@@ -0,0 +1,11 @@
+import { MultiType } from "./MultiType";
+import { Radar } from "./Radar";
+
+export default function AnalyticsPage() {
+    return (
+        <div className="h-screen p-4 w-full">
+            <MultiType />
+            <Radar />
+        </div>
+    )
+};
