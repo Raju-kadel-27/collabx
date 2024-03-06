5b3b0512c3ec493fc978667549fd307fe5fc52b2
Raju-github-profile <rajukadel.github@gmail.com>
2024-03-06 23:54:55 +0545

    feat: added polling

---
+++ apps/web/src/features/polling/components/page.tsx
@@ -0,0 +1,13 @@
+import PollFeed from './poll-feed';
+// import PollAnalytics from './poll-analytics';
+// import PollSubmitModal from './poll-submit-modal';
+
+const PollPage = () => {
+    return (
+        <div className='overflow-y-auto h-screen'>
+            <PollFeed />
+        </div>
+    )
+}
+
+export default PollPage;
