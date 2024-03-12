0a8cfa84fe59bac1e76e4ff5ed43bc6ed55aa8dc
Raju-github-profile <rajukadel.github@gmail.com>
2024-03-12 21:08:15 +0545

    added redis.ts

---
+++ backend/backend-ws/src/redis/Redis.ts
@@ -0,0 +1,37 @@
+import type { RedisClientType } from 'redis';
+import { createClient } from "redis";
+
+import { REDIS_URL, NOTIFICATION_QUEUE } from "../config"
+
+export class Redis {
+    private client: RedisClientType;
+    private static instance: Redis;
+
+    constructor() {
+        this.client = createClient({
+            url: REDIS_URL
+        });
+
+        this.client.connect();
+    }
+
+    public static getInstance(): Redis {
+        if (!this.instance) {
+            this.instance = new Redis();
+        }
+        return this.instance;
+    }
+    
+    async send(message: string) {
+        await this.client.rPush(NOTIFICATION_QUEUE, message);
+    }
+}
+
+
+
+
+
+
+
+
+
