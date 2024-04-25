0a2d2f9030832dcda5fe7f28892834de10924f99
Raju-github-profile <rajukadel.github@gmail.com>
2024-04-25 19:00:00 +0545

    feat: Receiving chat message from signaling

--- apps/web/src/features/text-chat/components/ScrollableChat.tsx
+++ apps/web/src/features/text-chat/components/ScrollableChat.tsx
@@ -3,64 +3,78 @@ import {
     isSameSender,
     isSameSenderMargin,
     isSameUser,
-} from "../helpers/ChatLogics";
+} from "../utils/ChatLogics";
 import { useGetAllMessagesQuery } from "../redux/apis/messageApiSlice";
 import { textChatSelector } from "../redux/slices/ChatSlice";
 import { useSelector } from "react-redux";
-import { userSelector } from "../../authentication/redux/slices/userSlice";
+import { tokenSelector, userSelector } from "../../authentication/redux/slices/userSlice";
 import { Avatar } from "@chakra-ui/avatar";
 import { Tooltip } from "@chakra-ui/tooltip";
 import PulseLoader from 'react-spinners/PulseLoader';
+import { useEffect, useState } from "react";
+import { SignalingManager } from "@/features/websocket/SignalingManager";
 
 const ScrollableChat = () => {
 
+    const [txtMsg,setTxtMsg]=useState<any>([])
+
+    const jwtToken = useSelector(tokenSelector);
     const user = useSelector(userSelector, (prev, next) => prev === next);
-
     const { selectedChat } = useSelector(textChatSelector, (prev, next) => prev === next);
-
-    console.log({ selectedChat })
-
-    const { data: messages,
-        isLoading: isMessageLoading,
-        isError: isMessageError,
-        error: messageError,
-        isSuccess: isMessageSuccess
-    } = useGetAllMessagesQuery(selectedChat._id, {
+    console.log({ selectedChat },"#######################");
+    const { data: messages} = useGetAllMessagesQuery(selectedChat._id, {
         skip: !selectedChat._id,
         refetchOnFocus:true
     });
+    console.log({ user, messages},"#######################");
 
-    console.log({ user, messages, isMessageError, isMessageSuccess, isMessageLoading, messageError });
+    useEffect(()=>{
+        if(messages?.length){
+            setTxtMsg(messages);
+        }
+    },[messages]);
+
+  useEffect(() => {
+        SignalingManager.getInstance().updateUuid(user._id, jwtToken)
+        //@ts-ignore
+        console.log("############################################ INITIALIZED ##################################################");
+        SignalingManager.getInstance().signaling?.on("CHAT_MESSAGE",(payload)=>{
+            console.log({payload},"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            const wsMsg=payload?.messages[0];
+            console.log({wsMsg});
+            setTxtMsg((prev:any)=>[...prev,wsMsg]);
+        })
+    }, []);
 
     return (
         <div className="w-full pl-1 h-full">
-            {messages?.length > 0 ?
-                messages?.map((m, i) => (
-                    <div className="overflow-y-auto w-full" style={{ display: "flex", justifyContent: 'start' }} key={m._id}>
-                        {(isSameSender(messages, m, i, user._id) ||
+            {txtMsg?.length > 0 ?
+            //@ts-ignore
+                txtMsg?.map((m, i) => (
+                    <div className="overflow-y-auto w-full" style={{ display: "flex", justifyContent: 'start' }} key={i}>
+                        {/* {(isSameSender(messages, m, i, user._id) ||
                             isLastMessage(messages, i, user._id)) && (
-                                <Tooltip label={m.sender.fullName} placement="bottom-start" hasArrow>
+                                <Tooltip label={m.sender.name} placement="bottom-start" hasArrow>
                                     <Avatar
                                         mt="7px"
                                         mr={1}
                                         size="sm"
                                         cursor="pointer"
-                                        name={m.sender.fullName}
+                                        name={m.sender.name}
                                         src={m.sender.pic}
                                     />
                                 </Tooltip>
-                            )}
+                            )} */}
                         <span
-                            className={`w-fit rounded-md mt-1 px-2 p-1 text-[13px] ${m.sender._id === user._id ? 'bg-green-400' : 'bg-slate-100 text-slate-700'}`}
-                            style={{
-                                marginLeft: isSameSenderMargin(messages, m, i, user._id),
-                                marginTop: isSameUser(messages, m, i, user._id) ? 3 : 10,
-                                overflow: 'hidden'
-                            }}
+                            // className={`w-fit rounded-lg mt-1 px-2 p-1 text-[13px] ${m.sender._id === user._id ? 'bg-blue-600 text-white' : 'bg-slate-100 text-slate-800'}`}
+                            // style={{
+                            //     marginLeft: isSameSenderMargin(messages, m, i, user._id),
+                            //     //@ts-ignore
+                            //     marginTop: isSameUser(messages, m, i, user._id) ? 3 : 10,
+                            //     overflow: 'hidden'
+                            // }}
                         >
-
-                            {m.content}
-
+                            {m.message}
                         </span>
                     </div>
                 )) :
@@ -69,12 +83,8 @@ const ScrollableChat = () => {
                         <PulseLoader speedMultiplier={0.6} size={16} color="blue" />
                     </div>
                 )
-
             }
-
         </div>
     );
 };
-
-
 export default ScrollableChat;
