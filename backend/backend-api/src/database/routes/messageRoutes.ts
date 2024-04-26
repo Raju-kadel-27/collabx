065633ff49fba1c3988fcc25750ecd502282375d
Raju-github-profile <rajukadel.github@gmail.com>
2024-04-26 17:23:10 +0545

    feat: added new-endpoint in messageroutes

--- backend/backend-api/src/database/routes/messageRoutes.ts
+++ backend/backend-api/src/database/routes/messageRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-// import { CelebrateMiddleware } from "../../middlewares/celebrate";
 import { MessageController } from "../controllers";
+// import { CelebrateMiddleware } from "../../middlewares/celebrate";
 // import { getAllMessageSchema, sendMessageSchema } from "../../validators/message";
 
 const route = Router()
@@ -12,12 +12,4 @@ export default (app: Router) => {
     route.get('/info/:messageId', MessageController.getMessageInfo);
     route.post('/delete', MessageController.deleteMessage);
     route.post('/sendmessage', MessageController.sendMessage);
-
-    // /api/message/delete
 }
-// /message/info/${messageId}
-// /api/message/sendmessage
-
-// /api/message/getmessages/658d9fd9f71dd80f13a83617
-
-// /api/messages/delete
